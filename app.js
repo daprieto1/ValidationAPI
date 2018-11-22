1720402f8ddf26ad1e4ff9d9b01dc235e6a95373
daprieto1 <da.prieto1@uniandes.edu.co>
2018-11-22 12:52:46 +0100

    fix port

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ expressSwagger(options)
 var users = require('./routes/users');
 app.use(`${basePath}/users`, users);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening!');
 });
